4dcc16d55dc7f7a7d0a4ec1c1f4976473140129c
Valery Zubkov <bouddha@mail.ru>
2022-08-10 22:23:56 +0400

    homework number 3

---
+++ HOMEWORKS/Homework_003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("Задайте номер четверти декартовой системы координат: ");
+int number = int.Parse(Console.ReadLine());
+string range = "";
+if(number == 1) range = "x>0, y>0";
+if(number == 2) range = "x<0, y>0";
+if(number == 3) range = "x<0, y<0";
+if(number == 4) range = "x>0, y<0";
+Console.WriteLine("Диапазон координат (x,y) для " + number + "-й четверти будет принимать следующее значение: " + range +".");
