f0c63bf58f5c9a42c5a3e1c208e8fed7952bcae7
Valery Zubkov <bouddha@mail.ru>
2022-08-11 19:36:43 +0400

    homework number 7

---
+++ HOMEWORKS/Homework_007/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+int digitNumber = 1;
+while(number / 10 >= 1 || number / 10 <= -1)
+{
+    number = number / 10;
+    digitNumber = digitNumber + 1;
+}
+Console.WriteLine("Введенное Вами число состоит из " + digitNumber + " цифр(ы).");
