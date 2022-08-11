93c51ec04be4ac3c9861831f6a41a9e2fd31fb24
Valery Zubkov <bouddha@mail.ru>
2022-08-11 17:30:56 +0400

    homework number 5

---
+++ HOMEWORKS/Homework_005/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите целое число N: ");
+int number = int.Parse(Console.ReadLine());
+int indexCube = 1;
+Console.WriteLine("Таблица кубов чисел от 1 до " + number + ":");
+while (indexCube <= number)
+{
+    Console.WriteLine(indexCube * indexCube * indexCube);
+    indexCube++;
+}
+
+
