88644e81869f03996657b5fb3fb1a34fb7f07d90
Valery Zubkov <bouddha@mail.ru>
2022-08-11 18:38:38 +0400

    homework number 6

---
+++ HOMEWORKS/Homework_006/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму чисел от 1 до А
+Console.WriteLine("Введите целое число А: ");
+int numberA = int.Parse(Console.ReadLine());
+int sum = 1;
+int numberNext = sum + 1;
+while(numberNext <= numberA)
+{
+    sum = sum + numberNext;
+    numberNext++;
+}
+Console.WriteLine("Сумма чисел от 1 до " + numberA + " составляет: " + sum);
