b30df788b5ed40e30b711c87312d3919d74182ca
Valery Zubkov <bouddha@mail.ru>
2022-08-12 16:27:37 +0400

    finally fixed homework number 8

--- HOMEWORKS/Homework_008/Program.cs
+++ HOMEWORKS/Homework_008/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Подсчитать сумму цифр в числе
 Console.WriteLine("Введите любое целое число: ");
 int number = int.Parse(Console.ReadLine());
+
 int Digitsnumber(int numb)   // функция подсчета количества цифр в числе
 {
 int digitNumber = 1;
@@ -12,29 +13,32 @@ while(numb / 10 >= 1 || numb / 10 <= -1)
 return digitNumber;
 }
 
-int Digitindex(int number, int index) // функция находждения любой цифры числа
+int Digitindex(int number, int index) // функция нахождения любой цифры числа(работает в связке с функцией Digitsnumber)
 {
 int digitIndex = 0;
 int parametr = 1;        
 int count = 0;
-//index = 1;
-while(count < (Digitsnumber(number) - index)) //цифра 3 в условии работает при числе от 3-х цифр, сюда как раз и попадают такие числа; поменяв ее, мы можем найти любую цифру числа.
+while(count < (Digitsnumber(number) - index)) //  меняя index, мы можем найти любую цифру числа.
       {
       parametr = parametr * 10;  // найдена закономерность изменения переменной parametr при увеличении разряда чисел.
       count++;
       }
-    if(number > 99)                  //проверка на положительное-отрицательное
-    {
+if(number > 0)                  //проверка на положительное-отрицательное, т.к. будем считать сумму цифр не зависимо от знака числа.
+   {
     digitIndex = number / parametr % 10;
-    }
+   }
     else
-    {
+   {
     digitIndex = -number / parametr % 10;
-    } 
+   } 
 
 return digitIndex;
-
 }
-int ind = 5;
-int digit = Digitindex(number, ind);
-Console.WriteLine(digit);
+int sum = 0;
+int ind = 1;
+while(ind <= Digitsnumber(number))
+{
+sum = sum + Digitindex(number, ind);
+ind++;
+}
+Console.WriteLine("Сумма всех цифр введенного Вами числа составляет: " + sum);
