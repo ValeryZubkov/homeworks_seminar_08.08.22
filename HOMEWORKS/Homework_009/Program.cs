a44e89cec9508ab4a464b3ddbdfa0644a51991c3
Valery Zubkov <bouddha@mail.ru>
2022-08-12 16:47:36 +0400

    homework number 9

---
+++ HOMEWORKS/Homework_009/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Ведите число N: ");
+int number = int.Parse(Console.ReadLine());
+//int index = 0;
+int result = 1;
+int numberNext = result + 1;
+while(numberNext <= number)
+{
+  result = result * numberNext;
+  numberNext++;
+}
+Console.WriteLine("Результат произведения чисел от 1 до " + number + " равен: " + result);
